85c1209b1835c1dcb383cb110a3194a360e5206d
Jeremy Lee <jeremy.lee01@libertymutual.com>
2020-10-09 16:01:51 -0400

    my first Lambda and API Gateway

--- lib/cdk-workshop-n0310713-stack.ts
+++ lib/cdk-workshop-n0310713-stack.ts
@@ -1,18 +1,70 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import iam = require('@aws-cdk/aws-iam');
+import { ManagedPolicy } from '@aws-cdk/aws-iam';
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
+import { defaultsDeep } from 'lodash';
 
 export class CdkWorkshopN0310713Stack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  // Variable to hold Lambda role
+  lambdaIAM:iam.Role;
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopN0310713Queue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    let properties = defaultsDeep({}, props, {
+      tags: {
+        "name": "cdk-app",
+      }
+    });
+    super(scope, id, properties);
+
+    this.lambdaIAM = this.getLambdaHandlerIamRole(this);
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X, // execution environment
+      code: lambda.Code.asset('lambda'), // code loaded from the "lambda directory"
+      handler: 'hello.handler', // file is "hello", function is "handler"
+      role: this.lambdaIAM // LM Compliant Lambda Role
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopN0310713Topic');
+    const api = new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+      cloudWatchRole: false,
+      proxy: false,
+      endpointTypes: [apigw.EndpointType.REGIONAL]
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      principals: [new iam.AnyPrincipal()],
+      actions: [
+        "execute-api:Invoke",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:PutLogEvents",
+        "logs:GetLogEvents",
+        "logs:FilterLogEvents",
+      ],
+      resources: [api.arnForExecuteApi()]
+    })
+
+    const proxy = api.root.addProxy({
+      defaultIntegration: new apigw.LambdaIntegration(hello)
+    });
   }
+
+    /**
+     * Method that returns an IAM role with some managed policies on it for lambdas to comply with LM standards
+     */
+    getLambdaHandlerIamRole(scope:any){
+      return new iam.Role(scope, 'cdkWorkshopLambdaHanderRole', {
+        assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+        managedPolicies: [
+            ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+            ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaVPCAccessExecutionRole')
+        ],
+      })
+    }  
 }
